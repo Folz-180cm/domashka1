347916c8007c6c7ac2a57218f5ac937e520fe284
Ruslan <folzbuisness@gmail.com>
2022-08-31 23:16:19 +0200

    решил последние 2 задачи

---
+++ task6/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num % 2 == 0){
+    Console.WriteLine($"{num} - чётное число");
+}
+else{
+    Console.WriteLine($"{num} - нечётное число");
+}
